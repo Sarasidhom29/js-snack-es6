71a49ed08a05caebc8520f6b2b57f0fefba8a1cb
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-09 16:53:56 +0200

    creato array di object e inidividuato elemento più leggero

--- snack-1/script.js
+++ snack-1/script.js
@@ -4,3 +4,41 @@
 //Creare un array di oggetti:
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+
+const biciInGara = [
+    {
+        nome: 'Wilier 0 SLR',
+        peso: '6,5'
+    },
+
+    {
+        nome: 'Trek Emonda SLR',
+        peso: '4,65'
+    },
+
+    {
+        nome: 'Orbea Orca M10iLTD Replica',
+        peso: '6,7'
+    },
+
+    {
+        nome: 'Canyon Ultimate CF SL 8 Aero',
+        peso: '7,86' 
+    },
+
+]
+
+// Confronto peso
+
+let pesoMinore = biciInGara[0].peso;
+
+for (let i = 1; i < biciInGara.length; i++) {
+
+    if (biciInGara[i].peso < pesoMinore) {
+
+        pesoMinore = biciInGara[i].peso;
+    }
+
+}
+
+console.log(pesoMinore + ' ' + 'kg');
